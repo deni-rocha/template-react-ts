bd8e0aea3d3b1634da9f4fc9ec7db524dd1db571
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-11-11 16:49:17 -0300

    criacao do componente input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,28 @@
+import icon from '../../assets/Input/icon/icon.svg'
+
+interface Props {
+  placeholder: string
+  type: string
+  name: string
+}
+
+const Input = ({ type, placeholder, name }: Props) => {
+  return (
+    <div className='w-full relative flex'>
+      <input
+        type={type}
+        name={name}
+        className="h-9 rounded-md text-[#535353] placeholder:text-[#535353] pl-2 w-full"
+        placeholder={placeholder}
+      />
+      {type === 'password' ? 
+      <div className='absolute items-center justify-end h-9 flex w-full'>
+      <img src={icon} alt="" className='mr-2'/>
+    </div>
+    : ''
+    }
+    </div>
+  )
+}
+
+export default Input
