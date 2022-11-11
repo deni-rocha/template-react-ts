6363cdf66d14eb3ff36b69cb35483735f2becfbd
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-11-11 10:06:32 -0300

    criacao Login page

---
+++ src/views/Login/index.tsx
@@ -0,0 +1,12 @@
+const Login = () => {
+    return(
+        <section className="bg-[#E5E5E5] w-screen h-screen flex items-center justify-center">
+            <div className="bg-[#848484] w-96 h-96 flex justify-center">
+                <p className="text-white w-16 h-8 text-center">LOGO</p>
+                <h1 className="pt-10 text-xl text-white">Acessar o moov</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Login
