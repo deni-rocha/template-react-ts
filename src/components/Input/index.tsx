8eb696ac068c968f6daae1a2b80e38872f3640d3
nosli <eu.denilsonrocha@gmail.com>
2022-11-15 14:53:10 -0300

    formatacao do componente Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import icon from '../../assets/Input/icon/icon.svg'
 
 interface Props {
@@ -6,21 +7,22 @@ interface Props {
   name: string
 }
 
-const Input = ({ type, placeholder, name }: Props) => {
+const Input = ({ type, placeholder, name }: Props): JSX.Element => {
   return (
-    <div className='w-full relative flex'>
+    <div className="w-full relative flex">
       <input
         type={type}
         name={name}
         className="h-9 rounded-md text-[#535353] placeholder:text-[#535353] pl-2 w-full"
         placeholder={placeholder}
       />
-      {type === 'password' ? 
-      <div className='absolute items-center justify-end h-9 flex w-full'>
-      <img src={icon} alt="" className='mr-2'/>
-    </div>
-    : ''
-    }
+      {type === 'password' ? (
+        <div className="absolute items-center justify-end h-9 flex w-full">
+          <img src={icon} alt="" className="mr-2" />
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   )
 }
