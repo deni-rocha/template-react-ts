ce5233ef001707e60a97a5115dff0cc9ee721631
nosli <eu.denilsonrocha@gmail.com>
2022-11-15 14:53:30 -0300

    formatacao do componente Logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,14 +1,13 @@
+import React from 'react'
 import logo from '../../assets/Logo/logo.svg'
 
-const Logo = () => {
-  return(
-      <div className="relative w-20 h-20">
-        <div className='absolute'>
-        <img src={logo} alt="logo"/>
-        </div>
+const Logo = (): JSX.Element => {
+  return (
+    <div className="relative w-20 h-20">
+      <div className="absolute">
+        <img src={logo} alt="logo" />
       </div>
+    </div>
   )
 }
-
-
 export default Logo
