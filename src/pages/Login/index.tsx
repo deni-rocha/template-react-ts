bf0dcd59416ac8907feab573a4d83de7610a4958
nosli <eu.denilsonrocha@gmail.com>
2022-11-15 13:05:09 -0300

    formatacao no componente Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,8 @@
+import React from 'react'
 import Input from '../../components/Input'
 import Logo from '../../components/Logo'
 
-
-const Login = () => {
+const Login = (): JSX.Element => {
   return (
     <section className="bg-[#474747] w-screen h-screen flex items-center justify-center">
       <div
@@ -21,7 +21,7 @@ const Login = () => {
             <p className="font-bold">E-mail de acesso</p>
             <Input type="email" placeholder="Informação" name="email" />
             <p className="self-end text-sm pt-2 cursor-pointer">
-              Esqueceu a sua <strong>senha?</strong>{' '}
+              Esqueceu a sua <strong>senha?</strong>
             </p>
           </label>
           <label className="flex flex-col h-24">
@@ -31,7 +31,6 @@ const Login = () => {
               name="password"
               placeholder="****************"
             />
-            
           </label>
           <button className="rounded-xl h-16 bg-[#4C4C4C] font-bold">
             Fazer login
