c832d827efa23dfe863db67ba64416d863b01dbd
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-11-16 11:45:19 -0300

    correção no import do arquivo css tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Login from "./pages/Login";
-import './index.css'
-function App() {
+import React from 'react'
+import '../src/index.css'
+import Login from './pages/Login'
+
+const App = (): JSX.Element => {
   return (
     <div className="App">
-      <Login/>
+      <Login />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
