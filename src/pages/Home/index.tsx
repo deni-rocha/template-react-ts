073c23b333f06ad1265a2fd6fbda7f2e4317dcf1
Denilson Rocha <denilsonrocha_@outlook.com>
2023-01-25 14:28:12 -0300

    configuração completa

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,15 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const Home = (): JSX.Element => {
-  useEffect(() => {
-    const buscar = async (): Promise<void> => {
-      const res = await axios.get(`https://dealta2-api.vercel.app/api`)
-      console.log(res)
-    }
-
-    void buscar()
-  })
   return (
     <div className="h-screen w-screen from-red-600 to-red-500 bg-gradient-to-b">
-      <div className="bg-red-500 h-full w-5/6 flex py-4 justify-center mx-auto shadow-md">
-        <h1 className="text-5xl text-white font-bold">
-          Projeto com Typescript Eslint e Prettier Configurado
-        </h1>
+      <div className="bg-red-500 h-full w-5/6 items-center text-white flex py-4 justify-center mx-auto shadow-md">
+        <section className="w-[600px] justify-center h-60 flex flex-col items-center">
+          <h1 className="text-lg font-bold">
+            Projeto React com Typescript, Eslint e Prettier
+          </h1>
+          <p className="text-sm">&copy; Denilson Rocha</p>
+        </section>
       </div>
     </div>
   )
