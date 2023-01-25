92acbfc2823a6bc356a1ea92daf28d6c54ed155f
Denilson Rocha <denilsonrocha_@outlook.com>
2023-01-25 11:56:22 -0300

    clear trash

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const Home = (): JSX.Element => {
+  useEffect(() => {
+    const buscar = async (): Promise<void> => {
+      const res = await axios.get(`https://dealta2-api.vercel.app/api`)
+      console.log(res)
+    }
+
+    void buscar()
+  })
   return (
     <div className="h-screen w-screen from-red-600 to-red-500 bg-gradient-to-b">
       <div className="bg-red-500 h-full w-5/6 flex py-4 justify-center mx-auto shadow-md">
